e8787d2109101db20bd053210ce38f7b71994878
madeleinehhhh <madeleine_herritage@baylor.edu>
2025-04-06 22:31:49 -0500

    1.0.6

--- core-extensions.php
+++ core-extensions.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: AP Core Extensions
  * Description: This plugin contains extended functionality. <strong>It should always be activated</strong>.
- * Version:     1.0.5
+ * Version:     1.0.6
  * Author:      Madeleine Herritage
  * Author URI:  https://madeleine.dev
  *
--- inc/cpt-teammember.php
+++ inc/cpt-teammember.php
@@ -31,44 +31,27 @@ function ap_cpt_teammember() {
       // 'template_lock' => 'all',
 
       // This is where we add taxonomies to our CPT
-      'taxonomies' => array( 'tag' ),
+      'taxonomies' => array( 'post_tag' ),
 
 		)
 	);
 }
 add_action('init', 'ap_cpt_teammember');
 
-// /**
-//  * Add custom taxonomies
-//  *
-//  * Additional custom taxonomies can be defined here
-//  * https://codex.wordpress.org/Function_Reference/register_taxonomy
-//  */
 // function ap_teammember_customtaxonomies() {
-//   // Add new "Roles" taxonomy to Posts
-//   register_taxonomy('Role', 'post', array(
-//     // Hierarchical taxonomy (like categories)
-//     'hierarchical' => true,
-//     // This array of options controls the labels displayed in the WordPress Admin UI
-//     'labels' => array(
-//       'name' => _x( 'Roles', 'taxonomy general name' ),
-//       'singular_name' => _x( 'Role', 'taxonomy singular name' ),
-//       'search_items' =>  __( 'Search Roles' ),
-//       'all_items' => __( 'All Roles' ),
-//       'parent_item' => __( 'Parent Role' ),
-//       'parent_item_colon' => __( 'Parent Role:' ),
-//       'edit_item' => __( 'Edit Role' ),
-//       'update_item' => __( 'Update Role' ),
-//       'add_new_item' => __( 'Add New Role' ),
-//       'new_item_name' => __( 'New Role Name' ),
-//       'menu_name' => __( 'Roles' ),
-//     ),
-//     // Control the slugs used for this taxonomy
-//     'rewrite' => array(
-//       'slug' => 'Roles', // This controls the base slug that will display before each term
-//       'with_front' => false, // Don't display the category base before "/Roles/"
-//       'hierarchical' => true // This will allow URL's like "/Roles/boston/cambridge/"
-//     ),
-//   ));
+//     $labels = array(
+//         'name'              => _x( 'Roles', 'taxonomy general name', 'my-text-domain' ),
+//         'singular_name'     => _x( 'Role', 'taxonomy singular name', 'my-text-domain' ),
+//         // ... other labels
+//     );
+
+//     $args = array(
+//         'labels'            => $labels,
+//         'hierarchical'      => true, // or false for non-hierarchical
+//         'public'            => true,
+//         'rewrite'           => array( 'slug' => 'my-custom-taxonomy' ),
+//         // ... other arguments
+//     );
+//     register_taxonomy( 'ap_teammember_customtaxonomies', array( 'my_custom_post_type' ), $args ); // Pass the custom post type name here
 // }
-// add_action( 'init', 'ap_teammember_customtaxonomies', 0 );
+// add_action( 'init', 'ap_teammember_customtaxonomies' );
