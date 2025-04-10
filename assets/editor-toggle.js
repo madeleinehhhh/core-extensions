647b8ff98266303c484b9769dca2d86b51380217
madeleinehhhh <madeleine_herritage@baylor.edu>
2025-04-09 22:09:36 -0500

    1.0.8

--- assets/editor-toggle.js
+++ assets/editor-toggle.js
@@ -2,22 +2,29 @@ const { registerPlugin } = wp.plugins;
 const { PluginDocumentSettingPanel } = wp.editPost;
 const { ToggleControl } = wp.components;
 const { useSelect, useDispatch } = wp.data;
+const { createElement: el } = wp.element;
 
 const ContactInfoToggle = () => {
-  const meta = useSelect((select) => select("core/editor").getEditedPostAttribute("meta"));
+  const postType = wp.data.select("core/editor").getCurrentPostType();
+  if (postType !== "ap_team") return null;
 
+  const meta = useSelect((select) => select("core/editor").getEditedPostAttribute("meta"));
   const { editPost } = useDispatch("core/editor");
 
-  return (
-    <PluginDocumentSettingPanel name="ap-team-contact-toggle" title="Team Member Options" className="ap-team-contact-toggle">
-      <ToggleControl
-        label="Display Contact Info?"
-        checked={!!meta.ap_show_contact_info}
-        onChange={(value) => {
-          editPost({ meta: { ap_show_contact_info: value } });
-        }}
-      />
-    </PluginDocumentSettingPanel>
+  return el(
+    PluginDocumentSettingPanel,
+    {
+      name: "ap-team-contact-toggle",
+      title: "Team Member Options",
+      className: "ap-team-contact-toggle",
+    },
+    el(ToggleControl, {
+      label: "Display Contact Info?",
+      checked: !!meta.ap_show_contact_info,
+      onChange: (value) => {
+        editPost({ meta: { ap_show_contact_info: value } });
+      },
+    })
   );
 };
 
